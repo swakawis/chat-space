f2ed95aff2a49845b2c84128d22e088b4eb0e7b8
swakawis <62795565+swakawis@users.noreply.github.com>
2020-09-17 14:51:02 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock 'capistrano (3.14.1)'
 set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:swakawis/chatspace.git'
+set :repo_url,  'git@github.com:swakawis/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
@@ -16,7 +16,7 @@ set :rbenv_ruby, '2.6.5' #カリキュラム通りに進めた場合、2.6.5で�
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/key_pem.pem'] 
+                  keys: ['~/.ssh/chatspace.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
